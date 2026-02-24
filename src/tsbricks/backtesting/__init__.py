db555a8bb42a45330f253ff2931da8820af00480
Elizabeth H. Camp <betsy.h.camp@gmail.com>
2026-02-23 22:18:15 -0800

    feat:
    backtesting evaluate_metrics(), add to export in backtesting/ __init__.py

--- src/tsbricks/backtesting/__init__.py
+++ src/tsbricks/backtesting/__init__.py
@@ -1,7 +1,9 @@
 from tsbricks.backtesting.cross_validation import generate_folds
+from tsbricks.backtesting.evaluation import evaluate_metrics
 from tsbricks.backtesting.schema import parse_config
 
 __all__ = [
+    "evaluate_metrics",
     "generate_folds",
     "parse_config",
 ]
